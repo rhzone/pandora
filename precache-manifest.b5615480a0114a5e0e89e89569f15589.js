689004e8a592008ce9d76dfcc3f456b495ff7eb6
rhzone <30968676+rhzone@users.noreply.github.com>
2021-01-09 13:55:34 +0000

    Deploying to gh-pages from master 7cfcbabbfbe063de8e62d092c39b6721b15e8922

---
+++ precache-manifest.b5615480a0114a5e0e89e89569f15589.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9883c33226388c96a1a",
+    "url": "/pandora/css/app.56a155f9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pandora/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "eb0b4a28f735ed60aa6d8a881f700d7a",
+    "url": "/pandora/index.html"
+  },
+  {
+    "revision": "5d1f0c108b166e967bd2",
+    "url": "/pandora/js/about.f339f586.js"
+  },
+  {
+    "revision": "e9883c33226388c96a1a",
+    "url": "/pandora/js/app.58ba208e.js"
+  },
+  {
+    "revision": "c55dde8e317bc5d27a4e",
+    "url": "/pandora/js/chunk-vendors.7c042cf8.js"
+  },
+  {
+    "revision": "9e605a1ab80f8bab43b5b58b39fb8e9a",
+    "url": "/pandora/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pandora/robots.txt"
+  }
+]);
